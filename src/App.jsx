ba7c0f521b06b627c6ba938ac9007ab298b32032
maimuna-t-333 <maimuna.tabr@gmail.com>
2026-08-14 14:37:06 +0600

    Updates: 2026-08-14T08:37:04.741Z

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ async function deleteTodo(id){
   const response=await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
     method: 'DELETE'
   });
-  
+  throw new Error('Delete failed');
   return id;
 }
 
